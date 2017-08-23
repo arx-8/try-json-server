6d88e384ba8babc386d9711e326a1e19c14fbb08
swan0 <swans.randomness@gmail.com>
2017-08-24 00:33:56 +0900

    Add editorconfig & refactoring

--- src/static/exec_json-server.sh
+++ src/static/exec_json-server.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 node ./node_modules/json-server/bin/index.js \
-    ./src/static/db.json \
-    --watch \
-    --port 3001 \
-    --delay 1000
+  ./src/static/db.json \
+  --watch \
+  --port 3001 \
+  --delay 1000
