ace8f3d7d45a5fcee02ce7d02d8957b005a0c611
swan0 <swans.randomness@gmail.com>
2017-08-24 01:06:15 +0900

    Divide data file

--- src/dynamic/my-json-server.js
+++ src/dynamic/my-json-server.js
@@ -2,11 +2,24 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const fs = require('fs')
 const middlewares = jsonServer.defaults()
 
+// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
+
+// Load data
+const data = []
+data.push(JSON.parse(fs.readFileSync(path.join(__dirname, 'data/comments.json'))))
+data.push(JSON.parse(fs.readFileSync(path.join(__dirname, 'data/profile.json'))))
+data.push(JSON.parse(fs.readFileSync(path.join(__dirname, 'data/posts.json'))))
+const router = jsonServer.router(Object.assign({}, ...data))
 server.use(router)
-server.listen(3000, () => {
+
+const PORT = 3001
+server.listen(PORT, () => {
   console.log('JSON Server is running')
+  console.log('-'.repeat(10))
+  console.log(`http://localhost:${PORT}`)
+  console.log('-'.repeat(10))
 })
